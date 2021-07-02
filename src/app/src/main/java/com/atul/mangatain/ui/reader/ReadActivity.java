a1eced2c5205cdec60c14f0344c524b47a0e865f
atul <atulpatare99@gmail.com>
2021-07-02 12:01:14 +0530

    add style for read activity

--- src/app/src/main/java/com/atul/mangatain/ui/reader/ReadActivity.java
+++ src/app/src/main/java/com/atul/mangatain/ui/reader/ReadActivity.java
@@ -1,15 +1,16 @@
 package com.atul.mangatain.ui.reader;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.atul.mangatain.MTConstants;
+import com.atul.mangatain.MTPreferences;
 import com.atul.mangatain.R;
+import com.atul.mangatain.helpers.ThemeHelper;
 import com.atul.mangatain.model.Chapter;
 import com.atul.mangatain.networking.RMRepository;
 import com.atul.mangatain.ui.reader.adapter.PageAdapter;
@@ -25,22 +26,24 @@ public class ReadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(MTPreferences.getThemeMode(getApplicationContext()));
+        setTheme(ThemeHelper.getTheme(MTPreferences.getTheme(getApplicationContext())));
         setContentView(R.layout.activity_read);
+
         RMRepository repository = new ViewModelProvider(this).get(RMRepository.class);
         pages = new ArrayList<>();
 
         RecyclerView pageLayout = findViewById(R.id.pages_layout);
         pageLayout.setLayoutManager(new LinearLayoutManager(this));
-        pageAdapter = new PageAdapter(pages);
+        pageAdapter = new PageAdapter( pages);
         pageLayout.setAdapter(pageAdapter);
 
         Chapter chapter = getIntent().getParcelableExtra("chapter");
-        if (chapter != null)
+        if (chapter != null) {
             repository.pages(chapter).observeForever(page -> {
                 pages.addAll(page);
                 pageAdapter.notifyDataSetChanged();
             });
-        else
-            Log.d(MTConstants.DEBUG_TAG, "null");
+        }
     }
 }
